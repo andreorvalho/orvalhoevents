bfc9f6f2284f9c88f94fe15f72294de88eb20274
andreorvalho <andre.orvalho@gmail.com>
2019-04-28 21:07:57 +0200

    added final redirects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,16 @@
 class HomeController < ApplicationController
+  include ActionView::Helpers::UrlHelper
+
   before_action :authenticate_user!
+  before_action :redirect_to_fuego
 
   def index
 
+  end
+
+  private
+
+  def redirect_to_fuego
+    redirect_to '/fuegofest' if current_page?(root_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/fuegofest', to: 'home#index'
+
   root 'home#index'
+  get "*path" => redirect("/fuegofest")
 end
