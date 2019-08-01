2217a3f035a35ae32dc8b2af21f63b846a2f59d6
andreorvalho <andre.orvalho@gmail.com>
2019-08-01 20:08:38 +0200

    deactivate email

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/fuegofest', to: 'home#index'
-  get '/email', to: 'invitations#index'
-
-  post 'send', to: 'invitations#new'
 
   root 'home#index'
   get "*path" => redirect("/fuegofest")
